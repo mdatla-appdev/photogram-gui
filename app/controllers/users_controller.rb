7214239703029118313093b6bb0aa5c9cad683e1
Manasa Datla <mdatla@chicagobooth.edu>
2023-04-25 16:19:26 +0000

    Username page functional

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,18 @@ class UsersController < ApplicationController
     render ({ :template => "user_templates/index.html.erb"})
 
   end
+
+  def show 
+  url_username = params.fetch("path_username")
+  
+  matching_username = User.where ({ :username => url_username})
+
+  @the_user = matching_username.at(0)
+
+  # if the_user == nil 
+   # redirect_to ("/")
+ #  else
+    render ({ :template => "user_templates/show.html.erb"})
+ # end
+end
 end
