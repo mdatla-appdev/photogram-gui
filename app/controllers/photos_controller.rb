8d7e8e3413f938e16c900ef22e046b7fd2e57f62
Manasa Datla <mdatla@chicagobooth.edu>
2023-05-01 00:16:04 +0000

    Show Works

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,4 +9,16 @@ def index
   render({ :template => "photo_templates/index.html.erb"})
 end
 
+def show
+#  Parameters: {"path_id"=>"777"}
+
+url_id = params.fetch("path_id")
+
+matching_photos = Photo.where({ :id => url_id })
+
+@the_photo = matching_photos.at(0)
+
+  render({ :template => "photo_templates/show.html.erb"})
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ get("/users/:path_username", { :controller => "users", :action => "show"})
 
 get("/photos" , {:controller=> "photos" , :action => "index"})
 
+get("/photos/:path_id" , {:controller=> "photos" , :action => "show"})
+
 end
